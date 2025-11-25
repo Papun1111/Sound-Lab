3086fc67eead44d5db01aa61b50653fccc6ea9f2
Papun1111 <gohanmohapatra@gmail.com>
2025-11-25 09:38:16 +0530

    updated plasma

--- sound-lab-frontend/src/components/Plasma.tsx
+++ sound-lab-frontend/src/components/Plasma.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useRef } from 'react';
 import { Renderer, Program, Mesh, Triangle } from 'ogl';
 
@@ -172,6 +173,7 @@ export const Plasma: React.FC<PlasmaProps> = ({
     let raf = 0;
     const t0 = performance.now();
     const loop = (t: number) => {
+      // eslint-disable-next-line prefer-const
       let timeValue = (t - t0) * 0.001;
       if (direction === 'pingpong') {
         const pingpongDuration = 10;
